b8864278c88d9656888bf386f60ddd4de3ea109d
Abid Hasan Kiam <kiamhasan267@gmail.com>
2025-07-28 00:45:40 +0600

    test: testing generateMetadata

--- app/(visitors)/layout.tsx
+++ app/(visitors)/layout.tsx
@@ -2,6 +2,8 @@ import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
 import type React from 'react';
 
+
+
 export default function RootLayout({
   children,
 }: {
--- app/(visitors)/page.tsx
+++ app/(visitors)/page.tsx
@@ -5,6 +5,29 @@ import { TestimonialSection } from '@/components/home/testimonial-section';
 import { StatisticsSection } from '@/components/home/statistics-section';
 import { TeacherSection } from '@/components/home/teacher-section';
 
+export async function generateMetadata() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const settingsResponse = await fetch(
+    `${baseUrl}/api/settings`
+  );
+
+  if (!settingsResponse.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const settings = await settingsResponse.json();
+
+  return {
+    title: settings?.metaTitle,
+    description: settings?.metaDescription,
+    keywords: settings?.metaKeywords,
+    icons: {
+      icon: settings?.favicon,
+    },
+  };
+}
+
 export default function HomePage() {
   return (
     <main className="container mx-auto">
